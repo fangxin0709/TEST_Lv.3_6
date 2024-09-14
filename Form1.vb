671e243b1cee29ab51b0ac257a5187b1bbcd4e3c
fangxin0709 <167990114+fangxin0709@users.noreply.github.com>
2024-09-14 08:04:42 +0800

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,16 +1,20 @@
-﻿Public Class Form1
+﻿Imports System.Numerics
+Imports System.Reflection
+
+Public Class Form1
     Dim d(100, 4), idx, err
     Sub rdata()
         FileOpen(6, "C:\Users\dora0\Desktop\6.txt", OpenMode.Input)
         idx = 0
         Do While Not EOF(6)
-            idx = idx + 1
+            idx += 1
             For i = 1 To 3
                 Input(6, d(idx, i))
             Next
         Loop
         FileClose(6)
     End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Call rdata()
         For i = 1 To idx
@@ -22,54 +26,51 @@
         Next
         Call wdata()
     End Sub
-
-    Sub sp1(ByVal i)
+    Sub sp1(i)
         Dim idno = d(i, 1)
         Dim m1 = Len(idno)
         If m1 <> 10 Then err = "FORMAT ERROR"
-
         Dim m2 = Mid(idno, 1, 1)
         If m2 < "A" Or m2 > "Z" Then err = "FORMAT ERROR"
-
-        For j = 2 To 10
+        For j = 2 To 8
             Dim m3 = Mid(idno, j, 1)
             If m3 < "0" Or m3 > "9" Then err = "FORMAT ERROR"
         Next
     End Sub
-    Sub sp2(ByVal i)
-        Dim sex_12 = Mid(d(i, 1), 2, 1)
+    Sub sp2(i)
+        Dim idno = d(i, 1)
+        Dim sex_12 = Mid(idno, 2, 1)
         Dim sex_MF = d(i, 3)
-
-        Dim msex = sex_12 & sex_MF
-        If msex <> "1M" And msex <> "2F" Then err = "SEX CODE ERROR"
+        Dim sex = sex_12 & sex_MF
+        If sex <> "1M" And sex <> "2F" Then
+            err = "SEX ERROR"
+        End If
     End Sub
-    Sub sp3(ByVal i)
-        Dim L1 = Mid(d(i, 1), 1, 1)
-        Dim s26 = "ABCDEFGHJKLMNPQRSTUVXYWZIO"
+    Sub sp3(i)
+        Dim idno = d(i, 1)
+        Dim L1 = Mid(idno, 1, 1)
+        Dim s26 = "ABCDEFGHJKLMNPQRSEUVWXYZIO"
         Dim m1 = InStr(s26, L1) + 9
-
         Dim x1 = m1 \ 10
         Dim x2 = m1 Mod 10
-
         Dim a(9)
         For j = 2 To 10
-            a(j - 1) = Mid(d(i, 1), j, 1)
+            a(j - 1) = Mid(idno, j, 1)
         Next
-
-        Dim y = x1 + 9 * x2
+        Dim y
+        y = x1 + 9 * x2
         For j = 1 To 8
             y = y + (9 - j) * a(j)
         Next
         y = y + a(9)
-
         If y Mod 10 <> 0 Then err = "CHECK SUM ERROR"
     End Sub
     Sub wdata()
         Dim table As New DataTable
-        table.Columns.Add("id_no")
-        table.Columns.Add("name")
-        table.Columns.Add("sex")
-        table.Columns.Add("error")
+        table.Columns.Add("ID_NO")
+        table.Columns.Add("NAME")
+        table.Columns.Add("SEX")
+        table.Columns.Add("ERROR")
         For i = 1 To idx
             Dim tr As DataRow = table.NewRow
             tr(0) = d(i, 1)
@@ -82,5 +83,8 @@
         dgv.Sort(dgv.Columns(0), 0)
         dgv.Columns(2).Width = 50
         dgv.Columns(3).Width = 150
+
+        dgv.Columns(2).Width = 50
+
     End Sub
 End Class
